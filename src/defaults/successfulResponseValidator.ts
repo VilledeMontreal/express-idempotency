eb003636d5c9da88884cbac8e9e4e8e365600d3e
JoscelynJean <joscelyn.jean@ville.montreal.qc.ca>
2020-06-09 09:16:04 -0400

    chore: prettier fix

--- src/defaults/successfulResponseValidator.ts
+++ src/defaults/successfulResponseValidator.ts
@@ -3,27 +3,27 @@
 // See LICENSE file in the project root for full license information.
 
 import {
-  IIdempotencyResponseValidator,
-  IdempotencyResponse,
-} from '../models/models';
+    IIdempotencyResponseValidator,
+    IdempotencyResponse,
+} from '../models/models'
 
 /**
  * Implementation of response validator.
  * Check for successful responses.
  */
 export class SuccessfulResponseValidator
-  implements IIdempotencyResponseValidator {
-  /**
-   * Check if the response is in the 2XX status code range and if it is, persist it.
-   * @param idempotencyResponse Response to validate
-   * @returns Indicate if need to persist
-   */
-  public isValidForPersistence(
-    idempotencyResponse: IdempotencyResponse
-  ): boolean {
-    return (
-      idempotencyResponse.statusCode >= 200 &&
-      idempotencyResponse.statusCode <= 299
-    );
-  }
+    implements IIdempotencyResponseValidator {
+    /**
+     * Check if the response is in the 2XX status code range and if it is, persist it.
+     * @param idempotencyResponse Response to validate
+     * @returns Indicate if need to persist
+     */
+    public isValidForPersistence(
+        idempotencyResponse: IdempotencyResponse
+    ): boolean {
+        return (
+            idempotencyResponse.statusCode >= 200 &&
+            idempotencyResponse.statusCode <= 299
+        )
+    }
 }
--- src/middleware/idempotency.ts
+++ src/middleware/idempotency.ts
@@ -2,34 +2,36 @@
 // Licensed under the MIT license.
 // See LICENSE file in the project root for full license information.
 
-import * as express from 'express';
-import { IdempotencyOptions } from '../models/models';
-import { IdempotencyService } from '../services/idempotencyService';
+import * as express from 'express'
+import { IdempotencyOptions } from '../models/models'
+import { IdempotencyService } from '../services/idempotencyService'
 
 export const ERROR_MSG_NOT_INITIALIZED =
-  'Idempotency service has not been initialized by the middleware.';
+    'Idempotency service has not been initialized by the middleware.'
 
 // Represent a valid function to use as a middleware for express
 type expressMiddleware = (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => Promise<void>;
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => Promise<void>
 
 // Keep a single instance of the service
-let idempotencyService: IdempotencyService = null;
+let idempotencyService: IdempotencyService = null
 
 export function getSharedIdempotencyService(): IdempotencyService {
-  if (idempotencyService) {
-    return idempotencyService;
-  }
-  throw new Error(ERROR_MSG_NOT_INITIALIZED);
+    if (idempotencyService) {
+        return idempotencyService
+    }
+    throw new Error(ERROR_MSG_NOT_INITIALIZED)
 }
 
 /**
  * Return a function used as a express middleware.
  */
-export const idempotency = (options?: IdempotencyOptions): expressMiddleware => {
-  idempotencyService = new IdempotencyService(options);
-  return idempotencyService.provideMiddlewareFunction;
-};
+export const idempotency = (
+    options?: IdempotencyOptions
+): expressMiddleware => {
+    idempotencyService = new IdempotencyService(options)
+    return idempotencyService.provideMiddlewareFunction
+}
